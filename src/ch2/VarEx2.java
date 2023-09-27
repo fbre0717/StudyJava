4fb8296bf47825222a439271949551855eda114d
fbre0717 <koh2040@naver.com>
2023-09-27 23:44:39 +0900

    ch2_VarEx2

---
+++ src/ch2/VarEx2.java
@@ -0,0 +1,18 @@
+package ch2;
+
+public class VarEx2 {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        int tmp = 0;
+
+        System.out.println("x:" + x + " y:" + y);
+
+        tmp = x;
+        x = y;
+        y = tmp;
+
+        System.out.println("x:" + x + " y:" + y);
+
+//        +를 이용하여 문자열을 합칠 수 있다.
+    }
+}
