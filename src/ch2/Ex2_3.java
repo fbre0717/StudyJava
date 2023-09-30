1c779ea1b8948fa71e0bbbc7a3bca6702ad2aba7
fbre0717 <koh2040@naver.com>
2023-09-30 23:58:02 +0900

    ch2_3

--- src/ch2/Ex2_3.java
+++ src/ch2/Ex2_3.java
@@ -11,12 +11,14 @@ public class Ex2_3 {
         System.out.println(" " + 7);
         System.out.println(7 + "");
         System.out.println("" + 7);
-        System.out.println("" + "");
+
         System.out.println(7 + 7 + "");
         System.out.println("" + 7 + 7);
         System.out.println();
         System.out.println("7" + 7 + "");
         System.out.println(7 + "7" + "");
-//        덧 셈 연산자는 피 연산자가 모두 숫자일 때는 두 수를 더하지만, 피연산자 중 어느 한 쪽이 String이면 나머지 한 쪽을 먼저 String으로 변한환 다음 두 String을 결합한다.
+//        number + number => add
+//        number + String => Concat
+//        String + String => Concat
     }
 }
